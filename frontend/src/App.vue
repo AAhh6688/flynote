9749681754e9b92dbaf794f07cebe445c90d2c79
AAhh6688 <suldanteruy@hotmail.com>
2026-02-01 04:00:48 +0800

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -17,24 +17,32 @@ import Navbar from './components/Navbar.vue'
 import Search from './components/Search.vue'
 import Playlist from './components/Playlist.vue'
 import Player from './components/Player.vue'
+import { searchMusic, getPlayUrl } from './api/index.js'
 
 // 全局音乐列表和当前播放歌曲
 const musicList = ref([])
 const currentMusic = ref(null)
 
-// 搜索歌曲（对接Search组件）
-const searchMusic = async (keyword) => {
-  const res = await import('./api/index.js').then(mod => mod.searchMusic(keyword))
+// 搜索歌曲（接收音源参数）
+const searchMusicHandler = async (keyword, source) => {
+  const res = await searchMusic(keyword, source)
   musicList.value = res.data
 }
 
-// 播放歌曲（对接Playlist组件）
-const playMusic = (music) => {
-  currentMusic.value = music
+// 播放歌曲（先获取播放链接）
+const playMusic = async (music) => {
+  // 获取播放链接
+  const playRes = await getPlayUrl(music)
+  if (playRes.code === 0 && playRes.data.url) {
+    // 给歌曲添加播放链接
+    currentMusic.value = { ...music, url: playRes.data.url }
+  } else {
+    ElMessage.error(playRes.msg || '获取播放链接失败')
+  }
 }
 
 // 暴露方法给Search组件
-defineExpose({ searchMusic })
+defineExpose({ searchMusic: searchMusicHandler })
 </script>
 
 <style scoped>
